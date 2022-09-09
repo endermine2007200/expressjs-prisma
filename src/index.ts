8e47ac4c0489ae1397f78585a99ce4edf97ca370
boxboy <96338953+endermine2007200@users.noreply.github.com>
2022-09-09 12:47:31 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,38 @@
-import express from "express";
+const express = require('express')
+const body = require('body-parser')
+const e = require('../main')
+const app = express()
+const fs = require('fs')
+let qrcode = require('qrcode')
+const port = process.env.PORT | 3000;
 
-const app = express();
-const port = process.env.PORT || 3000;
+let db = {}
+fs.writeFile('../db/db.json',JSON.stringify(db),(err)=>{})
 
-app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
+app.use(express.static("views"))
+app.use(body.raw())
+app.use(body.json())
+app.use(body.urlencoded({extended:true}))
 
-app.get("/", async (req, res) => {
-  res.send(
-    `
-  <h1>Todo REST API</h1>
-  <h2>Available Routes</h2>
-  <pre>
-    GET, POST /todos
-    GET, PUT, DELETE /todos/:id
-  </pre>
-  `.trim(),
-  );
+app.get('/', (req, res, next) => {
+	let re = fs.readFileSync(__dirname + '/../views/public/index.html',()=>{})
+	res.send((''+re).replace('#{id}',e.create()))
+});
+app.post('/qr', async (req,res)=>{
+	let qr = await e.get(req.body.id)
+	if(qr == 'error'|| !qr) res.end('error')
+	else{
+		res.setHeader('content-type','image/png')
+		res.send(await qrcode.toBuffer(qr))
+	}
+})
+app.get('/qr', async (req,res)=>{
+res.end('404 : method not true')
+})
+
+app.get('*', (req, res, next) => {
+	res.send('Sorry, page not found');
+	next();
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port)
