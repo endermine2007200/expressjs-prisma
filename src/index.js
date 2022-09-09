a7e698d67111586ed724f31f5cb9bd58548c54bd
boxboy <96338953+endermine2007200@users.noreply.github.com>
2022-09-09 13:15:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const e = require('../main')
 const app = express()
 const fs = require('fs')
 let qrcode = require('qrcode')
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 let db = {}
 fs.writeFile('../db/db.json',JSON.stringify(db),(err)=>{})
